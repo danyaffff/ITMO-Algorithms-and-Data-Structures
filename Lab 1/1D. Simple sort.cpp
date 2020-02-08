ffe8866293877b5c0ef20de16d646366b82379bb
Danya Khrapovitckii <40688428+danyaffff@users.noreply.github.com>
2020-02-09 01:10:55 +0300

    Update 1D. Simple sort.cpp

--- Lab 1/1D. Simple sort.cpp
+++ Lab 1/1D. Simple sort.cpp
@@ -5,15 +5,20 @@ int main() {
     int ArraySize, Array[10000];
     ifstream fin("smallsort.in");
     fin >> ArraySize;
-    for (int i = 0; i < ArraySize; i++)
+    for (int i = 0; i < ArraySize; i++) {
         fin >> Array[i];
+    }
     fin.close();
-    for (int i = 0; i < ArraySize - 1; i++)
-        for (int j = 0; j < ArraySize - i - 1; j++)
-            if (Array[j] > Array[j + 1])
+    for (int i = 0; i < ArraySize - 1; i++) {
+        for (int j = 0; j < ArraySize - i - 1; j++) {
+            if (Array[j] > Array[j + 1]) {
                 swap(Array[j], Array[j + 1]);
+            }
+        }
+    }
     ofstream fout("smallsort.out");
-    for (int i = 0; i < ArraySize; i++)
+    for (int i = 0; i < ArraySize; i++) {
         fout << Array[i] << " ";
+    }
     fout.close();
 }
