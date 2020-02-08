a0296edd4d81b892682d23aa0a683a8f558a20a4
Danya Khrapovitckii <40688428+danyaffff@users.noreply.github.com>
2020-02-09 01:11:42 +0300

    Update 1E. Acquaintance with the inhabitants of Sortland.cpp

--- Lab 1/1E. Acquaintance with the inhabitants of Sortland.cpp
+++ Lab 1/1E. Acquaintance with the inhabitants of Sortland.cpp
@@ -11,28 +11,34 @@ int main() {
         SortArray[i] = Array[i];
     }
     fin.close();
-    for (int i = 0; i < ArraySize - 1; i++)
-        for (int j = 0; j < ArraySize - i - 1; j++)
-            if (SortArray[j] > SortArray[j + 1])
+    for (int i = 0; i < ArraySize - 1; i++) {
+        for (int j = 0; j < ArraySize - i - 1; j++) {
+            if (SortArray[j] > SortArray[j + 1]) {
                 swap(SortArray[j], SortArray[j + 1]);
+            }
+        }
+    }
     Min = SortArray[0];
     Mid = SortArray[(ArraySize - 1) / 2];
     Max = SortArray[ArraySize - 1];
-    for (int i = 0; i < ArraySize; i++)
+    for (int i = 0; i < ArraySize; i++) {
         if (Min == Array[i]) {
             Min = ++i;
             break;
         }
-    for (int i = 0; i < ArraySize; i++)
+    }
+    for (int i = 0; i < ArraySize; i++) {
         if (Mid == Array[i]) {
             Mid = ++i;
             break;
         }
-    for (int i = 0; i < ArraySize; i++)
+    }
+    for (int i = 0; i < ArraySize; i++) {
         if (Max == Array[i]) {
             Max = ++i;
             break;
         }
+    }
     ofstream fout("sortland.out");
     fout << Min << " " << Mid << " " << Max;
     fout.close();
